8bfd490af8094c17e0aba5db71d30d6fa8d0d4ad
Dani Lyn <dlyn1110@gmail.com>
2018-03-15 18:56:17 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,8 @@
-def say_hello(name = "Hello Ruby Programmer!")
+def say_hello(name)
   puts "Hello Kent Beck!"
 
 end
 
-def say_hello(name)
+def say_hello(name = "Hello Ruby Programmer!")
   puts "Hello #{name}!"
 end
